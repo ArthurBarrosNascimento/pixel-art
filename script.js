1b657d3727394f7f2fc0577c7cb1d0a18c05392c
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-05-30 18:55:57 -0300

    Classe Selected feita e funcional nas cores

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function criarColuna() {
-    const linha = document.getElementById('pixel-board');
-    for (let index = 1; index <= 25; index += 1) {
+function criarColuna(tamanho) {
+    const linha = document.querySelector('#pixel-board');
+    for (let index = 1; index <= tamanho; index += 1) {
         const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
+        pixel.className = 'pixel';
         linha.appendChild(pixel);
+        pixel.addEventListener('click', function(){
+            let select = document.querySelector('.selected').style.backgroundColor;
+            pixel.style.backgroundColor = select;
+        })
     }
 }
 
-criarColuna();
+criarColuna(25);
 
-const black = document.getElementById('black').onclick;
+let recuperaColor = document.querySelectorAll('.color');
+for (let i = 0; i < recuperaColor.length; i += 1) {
+    recuperaColor[i].addEventListener('click', function(){
+        let recuperaSelect = document.querySelector('.selected');
+
+        recuperaSelect.classList.remove('selected');
+
+        recuperaColor[i].classList.add('selected');
+    });
+}
